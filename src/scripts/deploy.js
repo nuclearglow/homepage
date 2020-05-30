354eac6fdf7732dd999cc119d045718dc578b527
Sven Vowe <svenvopwe@gmail.com>
2020-05-30 14:13:49 +0200

    deployment prepared

---
+++ src/scripts/deploy.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const client = require('scp2')
+const SSH = require('simple-ssh')
+const getFolderSize = require('get-folder-size')
+
+const version = require('../package.json').version
+const credentials = require('../.credentials.json')
+
+/**
+ * Deployment: connect to SSH, copy dist folder to remote
+ */
+console.log(`Deploying svenvowe.de ${version}`)
+
+console.log('Cleaning up remote files')
+
+const ssh = new SSH({
+    host: credentials.remoteHost,
+    user: credentials.remoteUsername,
+    port: 22,
+    key: fs.readFileSync(credentials.privateKeyPath, 'utf8'),
+    passphrase: fs.readFileSync(credentials.privateKeyPassphrasePath, 'utf8').trim(),
+    baseDir: credentials.remotePath
+})
+
+// ssh.exec('rm -Rf *', {
+//     out: (stdout) => {
+//         console.log(stdout)
+//     },
+//     err: (stderr) => {
+//         console.log(stderr)
+//     }
+// }).start()
+// ssh.end()
+
+getFolderSize('dist', (err, size) => {
+    if (err) {
+        console.error(err)
+    }
+    const deploymentSize = (size / 1024 / 1024).toFixed(2)
+    console.log(`Uploading files (${deploymentSize} MB)`)
+})
+
+client.scp(
+    'dist/',
+    {
+        path: credentials.remotePath,
+        host: credentials.remoteHost,
+        username: credentials.remoteUsername,
+        port: 22,
+        privateKey: fs.readFileSync(credentials.privateKeyPath, 'utf8'),
+        passphrase: fs.readFileSync(credentials.privateKeyPassphrasePath, 'utf8').trim()
+    },
+    (err) => {
+        if (err) {
+            console.error('Error during SCP Deployment: ', err)
+        }
+        console.log('Deployment complete')
+    }
+)
