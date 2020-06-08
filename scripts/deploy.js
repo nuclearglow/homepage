9177df9773000fb007fb38305b77af43186d396c
Sven Vowe <svenvopwe@gmail.com>
2020-06-08 23:55:50 +0200

    deployment optimized

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,67 +1,72 @@
 const fs = require('fs')
-const client = require('scp2')
-const SSH = require('simple-ssh')
-const getFolderSize = require('get-folder-size')
+const du = require('du')
+const NodeSSH = require('node-ssh')
 
+const name = require('../package.json').name
 const version = require('../package.json').version
 const credentials = require('../.credentials.json')
 
 /**
  * Deployment: connect to SSH, copy dist folder to remote
  */
-console.log(`Deploying svenvowe.de ${version}`)
+console.log(`Deploying ${name}@${version}`)
 
-console.log('Cleaning up remote files')
+const deploy = async () => {
+    const ssh = new NodeSSH()
 
-const ssh = new SSH({
-    host: credentials.remoteHost,
-    user: credentials.remoteUsername,
-    port: 22,
-    key: fs.readFileSync(credentials.privateKeyPath, 'utf8'),
-    passphrase: fs.readFileSync(credentials.privateKeyPassphrasePath, 'utf8').trim(),
-    baseDir: credentials.remotePath
-})
+    console.log(`Connecting to ${credentials.remoteHost}`)
 
-// clean up directory
-ssh.exec('rm -Rf *.*', {
-    out: (stdout) => {
-        console.log(stdout)
-    },
-    err: (stderr) => {
-        console.log(stderr)
-    }
-}).start()
-
-ssh.on('error', (err) => {
-    console.log('Oops, something went wrong.')
-    console.log(err)
-    ssh.end()
-})
-
-ssh.end()
-
-getFolderSize('dist', (err, size) => {
-    if (err) {
-        console.error(err)
-    }
-    const deploymentSize = (size / 1024 / 1024).toFixed(2)
-    console.log(`Uploading files (${deploymentSize} MB)`)
-})
-
-client.scp(
-    'dist/',
-    {
-        path: credentials.remotePath,
+    await ssh.connect({
         host: credentials.remoteHost,
         username: credentials.remoteUsername,
         port: 22,
-        privateKey: fs.readFileSync(credentials.privateKeyPath, 'utf8'),
+        privateKey: credentials.privateKeyPath,
         passphrase: fs.readFileSync(credentials.privateKeyPassphrasePath, 'utf8').trim()
-    },
-    (err) => {
-        if (err) {
-            console.error('Error during Deployment: ', err)
-        }
-        console.log('Deployment complete')
+    })
+
+    console.log(`Cleaning up...`)
+
+    const result = await ssh.execCommand('rm -Rf *', { cwd: credentials.remotePath })
+    if (result.code > 0) {
+        console.err(result.stderr)
     }
-)
+
+    const size = await du('dist')
+    const deploymentSize = (size / 1024 / 1024).toFixed(2)
+
+    console.log(`Uploading files (${deploymentSize} MB)`)
+
+    const failed = []
+    const successful = []
+
+    const status = await ssh.putDirectory('dist/', credentials.remotePath, {
+        recursive: true,
+        concurrency: 3,
+        tick: (localPath, remotePath, error) => {
+            if (error) {
+                failed.push(localPath)
+            } else {
+                successful.push(localPath)
+            }
+        }
+    })
+
+    if (status) {
+        console.log('Deployment complete')
+        if (failed.length) {
+            console.err('failed transfers')
+            console.err(failed.join('\n'))
+        }
+    } else {
+        console.err('Deployment failed!')
+        if (failed.length) {
+            console.err('failed transfers')
+            console.err(failed.join('\n'))
+        }
+        process.exit(1)
+    }
+
+    ssh.dispose()
+}
+
+deploy()
